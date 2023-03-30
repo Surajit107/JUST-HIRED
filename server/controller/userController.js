fbbf1b2b45eecfb584ce3a81197710727a16078b
Surajit Pal <palsurajit06@gmail.cpm>
2023-03-31 01:22:07 +0530

    userControler

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,15 +2,20 @@ const User = require("../model/user");
 
 
 
-exports.userSignin = async (req, res)=>{
-    
+exports.userSignin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+
+    } catch (exc) {
+
+    }
 }
 
-exports.userSignup = async (req, res)=>{
-    const {full_name, email, phone, user_img, password} = req.body;
-    try{
-        
-    }catch(exc){
+exports.userSignup = async (req, res) => {
+    const { full_name, email, phone, user_img, password } = req.body;
+    try {
+
+    } catch (exc) {
 
     }
 }
