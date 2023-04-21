7da561579a1e1303321ad8e56094ea342d60add4
C-Halder <chayansantunu@gmail.com>
2023-04-22 01:43:11 +0530

    admin pages relocated

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,6 +1,53 @@
 const { jobCategoryModel } = require("../model/jobCategory");
 
 
+// dashboard
+exports.adminDashboard = (req, res) => {
+    res.render("./admin/admin_dashboard", {
+        title: "Admin dashboard",
+        // data: req.admin,
+        // message: req.flash("message")
+    })
+}
+
+// about
+exports.addAboutView = (req, res) => {
+    res.render("./admin/about", {
+        title: "About",
+        // data: req.admin,
+        // message: req.flash("message")
+    })
+}
+
+// blogs
+exports.addBlogView = (req, res) => {
+    res.render("./admin/blog", {
+        title: "Blog",
+        // data: req.admin,
+        // message: req.flash("message")
+    })
+}
+
+// post job view
+exports.postJobView = (req, res) => {
+    res.render("./admin/job", {
+        title: "Post job",
+        // data: req.admin,
+        // message: req.flash("message")
+    })
+}
+
+// add category view
+exports.addCategoryView = (req, res) => {
+    res.render("./admin/category", {
+        title: "Add category",
+        // data: req.admin,
+        // message: req.flash("message")
+    })
+}
+
+
+
 exports.getJobCategory = async (req, res) => {
     try {
         const allCategory = await jobCategoryModel.find();
